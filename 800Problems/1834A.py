2a7222a1aa6349f05461d9b1adead3bda63b6109
Md Faisal <98255055+faisal0006@users.noreply.github.com>
2025-01-08 23:04:50 +0530

    Create 1834A.py
    Solution of Unit Array in cf

---
+++ 800Problems/1834A.py
@@ -0,0 +1,24 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    Ncnt=0
+    Pcnt=0
+    for i in a:
+        if(i==-1):
+            Ncnt+=1
+        else:
+            Pcnt+=1
+    if(Ncnt>Pcnt):
+        opr=Ncnt-Pcnt
+        if(opr%2==0):
+            opr=opr//2
+        else:
+            opr=(opr+1)//2
+        if((Ncnt-opr)%2==1):
+            opr+=1
+        print(opr)
+    elif(Ncnt%2==1):
+        print(1)
+    else:
+        print(0)
